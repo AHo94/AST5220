211415f4b876e624b0dc0601f991090d2c005a24
Alex <hoalex94@gmail.com>
2017-01-19 14:03:32 +0100

    Adding constants and grid parameters to the program

--- Project1/Project1_python/Project1.py
+++ Project1/Project1_python/Project1.py
@@ -1,11 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Global constants
+# Units
+eV = 1.60217647e-19
+Mpc = 3.08568025e22
+
+# Cosmological parameters
+Omega_b = 0.046
+Omega_m = 0.224
+Omega_r = 8.3e-5
+Omega_nu = 0.0
+Omega_lambda = 1.0 - Omega_m - Omega_b - Omega_r - Omega_nu
+T_0 = 2.725
+n_s = 1.0
+A_s = 1.0
+h0 = 0.7
+H_0 = h0*100.0*1e3/Mpc
+
+# General constants
+c = 2.99792458e8
+epsilon_0 = 13.605698*eV
+m_e = 9.10938188e-31
+m_H = 1.673534e-27
+sigma_T = 6.652462e-29
+G_grav = 6.67258e-11
+rho_c = (3.0*H_0**2)/(8*np.pi*G_grav)
+alpha = 7.29735308e-3
+hbar = 1.05457148e-34
+k_b = 1.3806503e-23
+
+
 class time_mod():
 	def __init__(self):
-		# Defining constants
-		self.eV = 1.60217647e-19
+		self.n1 = 200
+		self.n2 = 300
+		self.n_t = self.n1 + self.n2
+
+		self.z_start_rec = 1630.4
+		self.z_end_rec = 614.2
+		self.z_0 = 0.0
+		self.x_start_rec = -np.log(1.0 + self.z_start_rec)
+		self.x_end_rec = -np.log(1.0 + self.z_end_rec)
+		self.x_0 = 0.0
+
+		self.n_eta = 1000
+		self.a_init = 1e-10
+		self.x_eta1 = np.log(self.a_init)
+		self.x_eta2 = 0
+
+		# Set up grid
+		self.x_t = np.linspace(self.x_start_rec, self.X_0, self.n_t)
+		self.a_t = np.linspace(self.a_init, 1, self.n_t)	# Scale factor today a_0 = 1
+
+	def Solve_Comformal_time(self):
 		
 
 solver = time_mod()
-print solver.eV
