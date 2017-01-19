d37dbb0f4b18a5b70bd80edd2df7d2c8154bc762
Alex <hoalex94@gmail.com>
2017-01-19 13:31:22 +0100

    Adding Python folder and python script

---
+++ Project1/Project1_python/Project1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class time_mod():
+	def __init__(self):
+		# Defining constants
+		self.eV = 1.60217647e-19
+		
+
+solver = time_mod()
+print solver.eV
