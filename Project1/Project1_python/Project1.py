e7b5f89071763d5f94a201734e4b65c18344bbb3
Alex <hoalex94@gmail.com>
2017-01-30 20:18:33 +0100

    Implemented Runge Kutta 4 method. Working on spline interpolation

--- Project1/Project1_python/Project1.py
+++ Project1/Project1_python/Project1.py
@@ -32,7 +32,10 @@ k_b = 1.3806503e-23
 
 
 class time_mod():
-	def __init__(self):
+	def __init__(self, h):
+		self.h = h 					# Step size for the RK4 method
+		self.RK_Prefactor = (h/6.0)	# Precalculating prefactor of RK4 method to reduce float point operations
+
 		self.n1 = 200
 		self.n2 = 300
 		self.n_t = self.n1 + self.n2
@@ -43,17 +46,92 @@ class time_mod():
 		self.x_start_rec = -np.log(1.0 + self.z_start_rec)
 		self.x_end_rec = -np.log(1.0 + self.z_end_rec)
 		self.x_0 = 0.0
+		self.a_start_rec = 1.0/(1.0 + self.z_start_rec)
+		self.a_end_rec = 1.0/(1.0 + self.z_end_rec)
 
 		self.n_eta = 1000
 		self.a_init = 1e-10
-		self.x_eta1 = np.log(self.a_init)
-		self.x_eta2 = 0
+		self.x_eta_init = np.log(self.a_init)
+		self.x_eta_end = 0
 
 		# Set up grid
-		self.x_t = np.linspace(self.x_start_rec, self.X_0, self.n_t)
-		self.a_t = np.linspace(self.a_init, 1, self.n_t)	# Scale factor today a_0 = 1
+		x_t_rec = np.linspace(self.x_start_rec, self.x_end_rec, self.n1)
+		x_t_today = np.linspace(self.x_end_rec, self.x_0, self.n2)
+		a_t_rec = np.linspace(self.a_start_rec, self.a_end_rec, self.n1)
+		a_t_today = np.linspace(self.a_end_rec, 1, self.n2)
+		# Merging the arrays into one
+		self.x_t = np.concatenate([x_t_rec, x_t_today])
+		self.a_t = np.concatenate([a_t_rec, a_t_today])
+		self.x_eta = np.linspace(self.x_eta_init, self.x_eta_end, self.n_t)	# X-values for the conformal time
+
+		# Set up array for the calculated variables
+		self.eta_array = np.zeros(self.n_t)
+
+	def Get_Hubble_param(self, x):
+		""" Function returns the Hubble parameter for a given x """
+		return H_0*np.sqrt((Omega_b + Omega_m)*np.exp(-3*x) + Omega_r*np.exp(-4*x) + Omega_lambda)
+
+	def Get_Hubble_prime(self, x):
+		""" Function returns the scaled Hubble parameter for a given x value. See report """
+		return H_0*np.sqrt((Omega_b + Omega_m)*np.exp(-x) + Omega_r*np.exp(-2*x) + Omega_lambda)
+
+	def Get_Hubble_prime_derivative(self, x):
+		""" Function returns the derivative of the scaled Hubble parameter. See report """
+		return -H_0**2*((Omega_b + Omega_m)*np.exp(-x) + 2*Omega_r*np.exp(-2*x))/(2*Get_Hubble_prime(x))
+
+	def RungeKutta4(self, i):
+		""" Function solving the fourth order Runge-Kutta method """
+		k1 = c/self.Get_Hubble_prime(self.x_eta[i])
+		k2 = c/self.Get_Hubble_prime(self.x_eta[i] + self.h/2.0)
+		k3 = c/self.Get_Hubble_prime(self.x_eta[i] + self.h/2.0)
+		k4 = c/self.Get_Hubble_prime(self.x_eta[i] + self.h)
+
+		return self.RK_Prefactor*(k1 + 2*k2 + 2*k3 + k4)
+
+	def Spline(self, eta, x, interval_start, interval_end):
+		""" Cubic spline interpolation. From Numerical recipies """
+		interpolated_points = np.zeros(interval_end - interval_start)
+		self.eta_d2 = self.Spline_second_derivative(eta, x, interval_start, interval_end)
+		for i in range(interval_start, interval_end):
+			"""
+			A = (x[i+1]-x)/(x[i+1] - x[i])
+			B = 1 - A
+			CD_factor = (1.0/6.0)*(x[i+1] - x[i])**2
+			C = CD_factor*(A**3 - A)
+			D = CD_factor*(B**3 - B)
+			"""
+			#interpolated_points[i] = A*eta[i] + B*eta[i+1] + C*eta_d2[i] + D*eta_d2[i+1]
+
+	def Spline_derivative(self):
+		a = 1
+
+	def Spline_second_derivative(self, y, x, index1, index2):
+		""" 
+		Second derivative of the spline interpolation.
+		Using natural cubic spline, i.e. zero double derivatives at the boundaries
+		"""
+		y_doublederivative = np.zeros(self.n_t)
+		for i in range(0, 10):
+			y_doublederivative[i+1] = (6.0/(x[i+1] - x[i]))*((y[i+1] - y[i])/(x[i+1] - x[i])\
+			 - (y[i] - y[i-1])/(x[i] - x[i-1]) - (x[i+1] - x[i-1])*y_doublederivative[i]/3.0\
+			 - (x[i] - x[i-1])*y_doublederivative[i-1]/6.0)
+		print y_doublederivative
+		return y_doublederivative
 
 	def Solve_Comformal_time(self):
-		
+		""" Solving the differential equation """
+		for i in range(1, self.n_t-1):
+			self.eta_array[i+1] = self.eta_array[i] + self.RungeKutta4(i)
 
-solver = time_mod()
+	def Plot_results(self):
+		""" Plotting the results """
+		self.Solve_Comformal_time()
+		self.Spline(self.eta_array, self.x_t, 0, self.n_t)
+		#print self.x_t
+		#print self.eta_d2
+
+		#plt.plot(self.x_t, self.eta_array)
+		#plt.show()
+
+solver = time_mod(1e-3)
+solver.Plot_results()
