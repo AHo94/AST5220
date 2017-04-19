913f2ee0c019965e1a9531a5b903770b89d9c54d
Alex <hoalex94@gmail.com>
2017-04-19 18:24:18 +0200

    Added class to compute end of TC. Added class to sort arrays when computed boltzmann equations. Fixed some stuff in TC equations, and more...

--- Project3/Project3_cpp/Project3/main.cpp
+++ Project3/Project3_cpp/Project3/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream> // Basic stuff
-#include <fstream>  // Used to writeo utfiles
+#include <fstream>  // Used to writeo outfiles
 #include <boost/array.hpp>  // Boost arrays
 #include <boost/numeric/odeint.hpp> // Boost odeint
 #include <boost/tuple/tuple.hpp>    // Boost tuple, used to return multiple values
@@ -45,7 +45,7 @@ struct Save_state_type{
 */
 void linspace(double start, double end, int NumberPts, vector<double> &array){
     // Creating a function equivalent to linspace from Numpy in Python
-    double step = (end-start)/(NumberPts-1);
+    double step = (end-start)/(double(NumberPts)-1);
     for (int i=0; i<NumberPts; i++){
         array[i] = start + i*step;}
 }
@@ -70,15 +70,18 @@ void Sort_variable_to_vector_SingleVar(vector<state_type> VariableArr, vector<do
 
 double Get_Hubble_param(double x0){
     // Function that returns the Hubble parameter
-    return H_0*sqrt((Omega_m + Omega_b)*exp(-3.0*x0) + Omega_r*exp(-4.0*x0) + Omega_lambda);}
+    double HubbleParam = H_0*sqrt((Omega_m + Omega_b)*exp(-3.0*x0) + Omega_r*exp(-4.0*x0) + Omega_lambda);
+    return HubbleParam;}
 
 double Get_Hubble_prime(double x0){
     // Function that returns the scaled Hubble parameter a*H
-    return H_0*sqrt((Omega_m + Omega_b)*exp(-x0) + Omega_r*exp(-2.0*x0) + Omega_lambda*exp(2.0*x0));}
+    double Hprime = H_0*sqrt((Omega_m + Omega_b)*exp(-x0) + Omega_r*exp(-2.0*x0) + Omega_lambda*exp(2.0*x0));
+    return Hprime;}
 
 double Get_Hubble_prime_derivative(double x0){
     // Function that returns the derivative of the scaled Hubble parameter
-    return H_0Squared*(0.5*(Omega_m + Omega_b) + 2.0*Omega_r + 2.0*Omega_lambda)/Get_Hubble_prime(x0);}
+    double HprimeDer = H_0Squared*(0.5*(Omega_m + Omega_b) + 2.0*Omega_r + 2.0*Omega_lambda)/Get_Hubble_prime(x0);
+    return HprimeDer;}
 
 boost::tuple<double, double, double, double> Get_omegas(double x0){
     // Computes the Omegas for a given 'time' x
@@ -97,7 +100,8 @@ void Diff_eq_eta(const state_type &eta, state_type &detadt, double x0){
 
 double Get_n_b(double x0){
     // Returns number density for baryons (or Hydrogen) for a given x
-    return n_bConst/exp(3.0*x0);}
+    double n_b = n_bConst/exp(3.0*x0);
+    return n_b;}
 
 double Saha_Equation(double x0){
     /* Solves saha equation. Only returns the positively valued solution of X_e.
@@ -105,10 +109,12 @@ double Saha_Equation(double x0){
        With a = 1, we can drop calculating the factor a and c.
     */
     double b = (Saha_b_factor/Get_n_b(x0))*exp(-EpsTemp_factor*exp(x0) - 3.0*x0/2.0);
-    if (0.5*(-b + sqrt(b*b + 4.0*b)) > 0){
-        return 0.5*(-b + sqrt(b*b + 4.0*b));}
-    else if (0.5*(-b - sqrt(b*b + 4.0*b)) > 0){
-        return 0.5*(-b - sqrt(b*b + 4.0*b));}
+    double SolPlus = (0.5)*(-b + sqrt(b*b + 4.0*b));
+    double SolMinus = (0.5)*(-b - sqrt(b*b + 4.0*b));
+    if (SolPlus > double(0.0)){
+        return SolPlus;}
+    else{
+        return SolMinus;}
 }
 void Peebles_equation(const state_type &X_e, state_type &dXedx, double x0){
     // Returns the right hand side of Peebles equation
@@ -130,11 +136,14 @@ void Compute_Xe(int n, double x_init, double x_0, vector<double> &ComputedX_e){
     vector<double> x_values(n);
     vector<state_type> X_e_temp;
     linspace(x_init, x_0, n, x_values);
-    ComputedX_e.push_back(1.0);
+    double X_e_INIT = 1.0;
+    double XeLimit = 0.97;
+    ComputedX_e.push_back(X_e_INIT);
     int EndI;
     for (int i=0; i<n; i++){
-        if (ComputedX_e[i] > 0.965){
-            ComputedX_e.push_back(Saha_Equation(x_values[i]));}
+        if (ComputedX_e[i] > XeLimit){
+            double xe = Saha_Equation(x_values[i]);
+            ComputedX_e.push_back(xe);}
         else{
             EndI = i;
             break;}
@@ -177,7 +186,31 @@ void TightCoupling_InitialCondition(double x0,  double k, state_type &InitialCon
     double Theta_1 = -c*k*Phi/(6.0*Hprime0);
     InitialCond = {Theta_0, Theta_1, delta_b, delta_b, v_b, v_b, Phi};
 }
+struct Get_TC_end{
+    /* Computes the time tight coupling ends.
+    * Tight coupling is when k/(Hprimed*tau') << 1, i.e is very small
+    * Assumes tight coupling end when k/(Hprimed*tau') > 0.1
+    */
+    vector<double> m_Taus;
+    vector<double> m_x_values;
+    spline1dinterpolant m_spline;
+    real_1d_array m_XInterp, m_TauInterp;
 
+    Get_TC_end(vector<double>&TauVec, vector<double>&x_values) : m_Taus(TauVec), m_x_values(x_values) {
+        m_XInterp.setcontent(m_x_values.size(), &(m_x_values[0]));
+        m_TauInterp.setcontent(m_Taus.size(), &(m_Taus[0]));
+        spline1dbuildcubic(m_XInterp, m_TauInterp, m_spline);}
+
+    double Get_xTCEnd(double k_value){
+        spline1dbuildcubic(m_XInterp, m_TauInterp, m_spline);
+        for (int i=0; i<m_Taus.size(); i++){
+            double ckHtau = c*k_value/(Get_Hubble_prime(m_x_values[i])*spline1dcalc(m_spline, m_x_values[i]));
+            if (fabs(ckHtau) > double(0.1)){
+                return m_x_values[i];
+            }
+        }
+    }
+};
 struct Solve_TightCoupling{
     // Class that sets up interpolation for tight coupling regime calculation
     vector<double> m_TauTC;
@@ -185,46 +218,53 @@ struct Solve_TightCoupling{
     double m_kTC;
     spline1dinterpolant m_splineTC;
     real_1d_array m_XInterp, m_TauInterp;
-    Solve_TightCoupling(vector<double>&TauVec, vector<double>&x_values, double &k_values)
+    Solve_TightCoupling(vector<double>TauVec, vector<double>x_values, double k_values)
         : m_TauTC(TauVec), m_xTC(x_values), m_kTC(k_values) {
         m_XInterp.setcontent(m_xTC.size(), &(m_xTC[0]));
         m_TauInterp.setcontent(m_TauTC.size(), &(m_TauTC[0]));
         spline1dbuildcubic(m_XInterp, m_TauInterp, m_splineTC);}
     void operator()(const state_type &Var, state_type &dVardx, double x0){
         // Solves Boltzmann equations for tight coupling
-        double Om_m, Om_b, Om_r, Om_lambda;
-        boost::tuple<double, double, double, double> Omegas = Get_omegas(x0);
-        Om_m = Omegas.get<0>();
-        Om_b = Omegas.get<1>();
-        Om_r = Omegas.get<2>();
-        Om_lambda = Omegas.get<3>();
+        double Theta_0 = Var[0];
+        double Theta_1 = Var[1];
+        double delta = Var[2];
+        double delta_b = Var[3];
+        double v = Var[4];
+        double v_b = Var[5];
+        double Phi = Var[6];
+
         double Hprimed = Get_Hubble_prime(x0);
         double HprimedDer = Get_Hubble_prime_derivative(x0);
-        //double Hprime_HPrimedDer = Hprimed/HprimedDer;
         double Hprime_HPrimedDer = HprimedDer/Hprimed;
         double ck_Hprimed = (c*m_kTC)/Hprimed;
         double Tau, TauDer, TauDoubleDer;
         spline1ddiff(m_splineTC, x0, Tau, TauDer, TauDoubleDer);
 
-        double R = 4.0*Om_r/(3.0*Om_m*exp(x0));
-        double Theta2 = -20.0*ck_Hprimed*Var[1]/(45.0*TauDer);
-
-        double Psi = -Var[6] - PsiPrefactor*Om_r*Theta2/(m_kTC*m_kTC*exp(2.0*x0));
+        double R = 4.0*Omega_r/(3.0*Omega_m*exp(x0));
+        double Theta_2 = -20.0*ck_Hprimed*Theta_1/(45.0*TauDer);
+        double Psi = -Phi - PsiPrefactor*Omega_r*Theta_2/(m_kTC*m_kTC*exp(2.0*x0));
         double ck_HPsi = ck_Hprimed*Psi;
-
-        dVardx[6] = Psi - ck_Hprimed*ck_Hprimed*Var[6]/3.0
-            + (H_0Squared/(2.0*Hprimed*Hprimed))*(Omega_m*Var[2]*exp(-x0) + Omega_b*Var[3]*exp(-x0)
-                            + 4.0*Omega_r*exp(-2.0*x0)*Var[0]);
-
-        dVardx[0] = -ck_Hprimed*Var[1] - dVardx[6];
-        double q = -(( (1.0-2.0*R)*TauDer + (1.0+R)*TauDoubleDer)*(3.0*Var[1] + Var[5]) - ck_HPsi
-                + (1.0-Hprime_HPrimedDer)*ck_Hprimed*(-Var[0] + 2.0*Theta2) - ck_Hprimed*dVardx[0])
+        dVardx[6] = Psi - ck_Hprimed*ck_Hprimed*Phi/3.0
+            + (H_0Squared/(2.0*Hprimed*Hprimed))*(Omega_m*delta*exp(-x0) + Omega_b*delta_b*exp(-x0)
+            + 4.0*Omega_r*exp(-2.0*x0)*Theta_0);
+        dVardx[0] = -ck_Hprimed*Theta_1 - dVardx[6];
+        double q = -(( (1.0-2.0*R)*TauDer + (1.0+R)*TauDoubleDer)*(3.0*Theta_1 + v_b) - ck_HPsi
+                + (1.0-Hprime_HPrimedDer)*ck_Hprimed*(-Theta_0 + 2.0*Theta_2) - ck_Hprimed*dVardx[0])
                 /((1.0+R)*TauDer + Hprime_HPrimedDer - 1);
-        dVardx[2] = ck_Hprimed*Var[4] - 3.0*dVardx[6];
-        dVardx[3] = ck_Hprimed*Var[5] - 3.0*dVardx[6];
-        dVardx[4] = -Var[4] - ck_HPsi;
-        dVardx[5] = (-Var[5] - ck_HPsi + R*(q + ck_Hprimed*(-Var[0] + 2.0*Theta2) - ck_HPsi))/(1.0+R);
+        dVardx[2] = ck_Hprimed*v - 3.0*dVardx[6];
+        dVardx[3] = ck_Hprimed*v_b - 3.0*dVardx[6];
+        dVardx[4] = -v - ck_HPsi;
+        dVardx[5] = (-v_b - ck_HPsi + R*(q + ck_Hprimed*(-Theta_0 + 2.0*Theta_2) - ck_HPsi))/(1.0+R);
         dVardx[1] = (q-dVardx[5])/3.0;
+        /*
+        dVardx[0] = dTheta0dx;
+        dVardx[1] = dTheta1dx;
+        dVardx[2] = dDeltadx;
+        dVardx[3] = dDelta_bdx;
+        dVardx[4] = dvdx;
+        dVardx[5] = dv_bdx;
+        dVardx[6] = dPhidx;
+        */
         /*
         cout << "----" << endl;
         cout << dVardx[0] << endl;
@@ -246,6 +286,62 @@ struct Solve_TightCoupling{
     }
 };
 
+struct MergeAndFinalize{
+    vector<double> m_Taus;
+    vector<double> m_x;
+    double m_k;
+    spline1dinterpolant m_spline;
+    real_1d_array m_XInterp, m_TauInterp;
+    MergeAndFinalize(vector<double> TauVec, vector<double> x_values, double k_values)
+        : m_Taus(TauVec), m_x(x_values), m_k(k_values) {
+        m_XInterp.setcontent(m_x.size(), &(m_x[0]));
+        m_TauInterp.setcontent(m_Taus.size(), &(m_Taus[0]));
+        spline1dbuildcubic(m_XInterp, m_TauInterp, m_spline);}
+    void MergeTC(vector<state_type> ComputedVar, vector<double> &Theta0, vector<double> &Theta1,
+                 vector<double> &Theta2, vector<double> &Theta3, vector<double> &Theta4, vector<double> &Theta5,
+                 vector<double> &Theta6, vector<double> &delta, vector<double> &deltab, vector<double> &v,
+                 vector<double> &vb, vector<double> &Phi, vector<double> x_TC, state_type &InitCond){
+        double TauHolder, TauDer, TauDDerHolder;
+        for (int i=0; i< ComputedVar.size()-1; i++){
+            double Hprime = Get_Hubble_prime(x_TC[i]);
+            spline1ddiff(m_spline, x_TC[i], TauHolder, TauDer, TauDDerHolder);
+            Theta0[i] = ComputedVar[i][0];
+            Theta1[i] = ComputedVar[i][1];
+            Theta2[i] = -20.0*c*m_k/(45.0*Hprime*TauDer);
+            Theta3[i] = -3.0*c*m_k/(7.0*Hprime*TauDer);
+            Theta4[i] = -4.0*c*m_k/(9.0*Hprime*TauDer);
+            Theta5[i] = -5.0*c*m_k/(11.0*Hprime*TauDer);
+            Theta6[i] = -6.0*c*m_k/(12.0*Hprime*TauDer);
+            delta[i] = ComputedVar[i][2];
+            deltab[i] = ComputedVar[i][3];
+            v[i] = ComputedVar[i][4];
+            vb[i] = ComputedVar[i][5];
+            Phi[i] = ComputedVar[i][6];
+        }
+        InitCond = {Theta0[-1], Theta1[-1], Theta2[-1], Theta3[-1], Theta4[-1], Theta5[-1], Theta6[-1],
+                   delta[-1], deltab[-1], v[-1], vb[-1], Phi[-1]};
+    }
+    void Finalize(vector<state_type> ComputedVar, vector<double> &Theta0, vector<double> &Theta1,
+                  vector<double> &Theta2, vector<double> &Theta3, vector<double> &Theta4, vector<double> &Theta5,
+                  vector<double> &Theta6, vector<double> &delta, vector<double> &deltab, vector<double> &v,
+                  vector<double> &vb, vector<double> &Phi){
+        for (int i=0; i<ComputedVar.size()-1; i++){
+            Theta0.push_back(ComputedVar[i][0]);
+            Theta1.push_back(ComputedVar[i][1]);
+            Theta2.push_back(ComputedVar[i][2]);
+            Theta3.push_back(ComputedVar[i][3]);
+            Theta4.push_back(ComputedVar[i][4]);
+            Theta5.push_back(ComputedVar[i][5]);
+            Theta6.push_back(ComputedVar[i][6]);
+            delta.push_back(ComputedVar[i][7]);
+            deltab.push_back(ComputedVar[i][8]);
+            v.push_back(ComputedVar[i][9]);
+            vb.push_back(ComputedVar[i][10]);
+            Phi.push_back(ComputedVar[i][11]);
+        }
+    }
+};
+
 void write_outfile(vector<double> x, vector<double> Value, string Value_name, string filename){
     // Saves data to text file.
     ofstream datafile;
@@ -256,17 +352,7 @@ void write_outfile(vector<double> x, vector<double> Value, string Value_name, st
     }
     datafile.close();
 }
-/*
-void Sort_Boltzmann_variables(vector<state_type> Input_vector, vector<double> &Theta0, vector<double> &Thet1,
-      vector<double> &Theta2, vector<double> &Theta3, vector<double> &Theta4, vector<double> &Theta5,
-      vector<double> &Theta6, vector<double> &delta, vector<double> &delta_b, vector<double> &v,
-      vector<double> &v_b, vector<double> &phi, int TightCoupling = 1){
-    // Sorting the boltzmann variables to their seperate
-    if (TightCoupling == 1){
 
-    }
-}
-*/
 void printstuf(const state_type &Var, double x0){
     cout << fixed;
     cout << setprecision(4) << x0 << '\t' << Var[0] << '\t' << Var[1] << '\t' << Var[2]
@@ -292,11 +378,8 @@ int main(int argc, char *argv[])
     int n_eta = 3000;
     double a_init = 1e-8;
     double x_init = log(a_init);
-
-    vector<double> x_TC(200);
-    vector<double> x_after_TC(300);
-    linspace(x_init, x_start_rec, n1, x_TC);
-    linspace(x_start_rec, x_end_rec, n2, x_after_TC);
+    vector<double> Full_x_grid(n_eta);
+    linspace(x_init, 0.0, n_eta, Full_x_grid);
 
     vector<double> k(100);
     double k_min = 0.1*H_0/c;
@@ -365,18 +448,45 @@ int main(int argc, char *argv[])
     for (int i=0; i<n_eta; i++){
         spline1ddiff(splineGs, x_tau[i], s_temp, gDer[i], gDoubleDer[i]);}
 
+    Get_TC_end ObtainXInstance(Taus, Full_x_grid);
+
+    double x_TC_end = ObtainXInstance.Get_xTCEnd(k[0]);
+    cout << x_TC_end << endl;
+
     vector<state_type> state_type_Temp;
-    vector<double> x_var;
+    vector<double> x_TC;
     state_type state_type_InitTC(6);
     TightCoupling_InitialCondition(x_init, k[0], state_type_InitTC);
     Solve_TightCoupling TCInstance(Taus, x_tau, k[0]);
-    cout << "Instance ok" << endl;
     timer = clock();
-    size_t EBTC_step = integrate_adaptive(bulst_step(), TCInstance, state_type_InitTC, x_init, x_start_rec,
-                       (x_start_rec-x_init)/(n_eta-1.0), printstuf);
-                       //Save_single_variable(state_type_Temp, x_var));
+
+    size_t EBTC_step = integrate_adaptive(bulst_step(), TCInstance, state_type_InitTC, x_init, x_TC_end,
+                       (x_TC_end-x_init)/(n_eta-1.0), // printstuf);
+                       Save_single_variable(state_type_Temp, x_TC));
+
+    MergeAndFinalize MergerInstance(Taus, Full_x_grid, k[0]);
+    vector<double> Theta0(EBTC_step);
+    vector<double> Theta1(EBTC_step);
+    vector<double> Theta2(EBTC_step);
+    vector<double> Theta3(EBTC_step);
+    vector<double> Theta4(EBTC_step);
+    vector<double> Theta5(EBTC_step);
+    vector<double> Theta6(EBTC_step);
+    vector<double> delta(EBTC_step);
+    vector<double> deltab(EBTC_step);
+    vector<double> v(EBTC_step);
+    vector<double> vb(EBTC_step);
+    vector<double> Phi(EBTC_step);
+    state_type Init_afterTC(12);
+    cout << "instance ok" << endl;
+    cout << x_TC.size() << endl;
+    cout << state_type_Temp.size() << endl;
+    MergerInstance.MergeTC(state_type_Temp, Theta0, Theta1, Theta2, Theta3, Theta4, Theta5, Theta6,
+                           delta, deltab, v, vb, Phi, x_TC, Init_afterTC);
+    cout << "instance ok" << endl;
     timer = clock() - timer;
     cout << "tot time " << float(timer)/CLOCKS_PER_SEC << endl;
     cout << "Number of datapoints for tight coupling: " << EBTC_step << endl;
+    cout << state_type_Temp.size() << endl;
     return 0;
 }
